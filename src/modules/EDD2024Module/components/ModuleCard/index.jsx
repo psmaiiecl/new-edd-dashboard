012e6417cfbee9ba4c70383769d392d2c41b7faf
psmaiiecl <pablo.maguina@iie.cl>
2025-04-21 12:36:30 -0400

    css corrections

--- src/modules/EDD2024Module/components/ModuleCard/index.jsx
+++ src/modules/EDD2024Module/components/ModuleCard/index.jsx
@@ -1,12 +1,16 @@
+import { StandardLoadingPanel } from "../../../../components/StandardLoadingPanel";
 import "./index.css";
 
-export function ModuleCard({ title, action, children }) {
+export function ModuleCard({ title, action, children, loading=false }) {
   return (
     <div className="module-card roboto-regular" onClick={action}>
       <div className="module-card__title">
         <span>{title}</span>
       </div>
-      <div className="module-card__content">{children}</div>
+      <div className="module-card__content">
+        {loading && <StandardLoadingPanel/>}
+        {children}
+        </div>
     </div>
   );
 }
